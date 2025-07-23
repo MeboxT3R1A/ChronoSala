ee5b1b0258635d233f8f110e3d1edb6cb9b25e45
Jota <torresjonatas02@gmail.com>
2025-07-23 15:21:05 -0300

    Estilos do coordenador.html está sendo carregado

--- app/db.py
+++ app/db.py
@@ -6,8 +6,8 @@ from flask import g
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "port": 3307,
-    "password": "senac",
+    "port": 3306,
+    "password": "lasanha",
     "database": "chronosala",
     "cursorclass": pymysql.cursors.DictCursor
 }
